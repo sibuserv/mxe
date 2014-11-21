b33860fb1599cb6d2b64dd0b608b2b1a032d3ff3
Boris Pek <tehnick-8@yandex.ru>
2014-11-21 18:46:53 +0300

    Use Qt5 in openscenegraph.
    Signed-off-by: Boris Pek <tehnick-8@yandex.ru>

--- src/openscenegraph.mk
+++ src/openscenegraph.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := 5c666531f7d487075fd692d89f1e05036306192a
 $(PKG)_SUBDIR   := OpenSceneGraph-$($(PKG)_VERSION)
 $(PKG)_FILE     := OpenSceneGraph-$($(PKG)_VERSION).zip
 $(PKG)_URL      := http://www.openscenegraph.org/downloads/developer_releases/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc boost curl dcmtk ffmpeg freetype gdal giflib gta jasper jpeg libpng openal openexr poppler qt tiff xine-lib zlib
+$(PKG)_DEPS     := gcc boost curl dcmtk ffmpeg freetype gdal giflib gta jasper jpeg libpng openal openexr poppler qt5 tiff xine-lib zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://www.openscenegraph.org/downloads/developer_releases/?C=M;O=D' | \
@@ -29,6 +29,7 @@ define $(PKG)_BUILD
         -DDYNAMIC_OPENSCENEGRAPH=OFF \
         -DBUILD_OSG_APPLICATIONS=OFF \
         -DPOPPLER_HAS_CAIRO_EXITCODE=0 \
+        -DDESIRED_QT_VERSION=5 \
         -D_OPENTHREADS_ATOMIC_USE_GCC_BUILTINS_EXITCODE=1 \
         -D_OPENTHREADS_ATOMIC_USE_WIN32_INTERLOCKED=1
     $(MAKE) -C '$(1).build' -j '$(JOBS)' install VERBOSE=1
