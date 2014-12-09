13ec0ad6df1f00387b02583ef471bc1f66df0568
Boris Pek <tehnick-8@yandex.ru>
2014-12-09 13:53:22 +0300

    gdal: use internal json-c library

--- src/gdal.mk
+++ src/gdal.mk
@@ -10,7 +10,7 @@ $(PKG)_FILE     := gdal-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://download.osgeo.org/gdal/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://ftp.remotesensing.org/gdal/$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc proj zlib libpng libxml2 tiff libgeotiff jpeg \
-                   giflib curl geos postgresql json-c
+                   giflib curl geos postgresql
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://trac.osgeo.org/gdal/wiki/DownloadSource' | \
@@ -35,7 +35,7 @@ define $(PKG)_CONFIGURE
         --with-geotiff='$(PREFIX)/$(TARGET)' \
         --with-jpeg='$(PREFIX)/$(TARGET)' \
         --with-gif='$(PREFIX)/$(TARGET)' \
-        --with-libjson-c='$(PREFIX)/$(TARGET)' \
+        --with-libjson-c=internal \
         --without-expat \
         --without-gta \
         --without-sqlite3 \
