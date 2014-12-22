1cb12bd03adeb330144e70e743c6a0efe13538f2
Boris Pek <tehnick-8@yandex.ru>
2014-12-22 13:28:31 +0300

    sqlite: use custom CFLAGS, CXXFLAGS and LDFLAGS

--- src/sqlite.mk
+++ src/sqlite.mk
@@ -17,7 +17,12 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure \
+    cd '$(1)' && \
+    CPPFLAGS="$(CPPFLAGS)" \
+    CFLAGS="$(CFLAGS)" \
+    CXXFLAGS="$(CXXFLAGS)" \
+    LDFLAGS="$(LDFLAGS)" \
+    ./configure \
         $(MXE_CONFIGURE_OPTS) \
         --disable-readline \
         --enable-threadsafe \
