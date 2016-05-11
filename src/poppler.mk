250a084cb547cb2ee7d532bdcf328cfbb5891a2c
Boris Pek <tehnick-8@yandex.ru>
2016-05-11 14:56:28 +0300

    poppler: depend on qtbase instead of qt5

--- src/poppler.mk
+++ src/poppler.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := b616ee869d0b1f8a7a2c71cf346f55c1bff624cce4badebe17f506ec8ce7d
 $(PKG)_SUBDIR   := poppler-$($(PKG)_VERSION)
 $(PKG)_FILE     := poppler-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://poppler.freedesktop.org/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc cairo curl freetype glib jpeg lcms libpng qt5 tiff zlib  
+$(PKG)_DEPS     := gcc cairo curl freetype glib jpeg lcms libpng qtbase tiff zlib  
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://poppler.freedesktop.org/' | \
