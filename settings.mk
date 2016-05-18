b39215752ef2dcd0fdb0829c165cc6c8355f8dad
Boris Pek <tehnick-8@yandex.ru>
2016-05-18 17:40:51 +0300

    Update settings.mk

--- settings.mk
+++ settings.mk
@@ -30,7 +30,7 @@ LDFLAGS  := -Wl,--gc-sections -Wl,--strip-all -Wl,--as-needed
 # The three lines below makes `make` build these "local
 # packages" instead of all packages.
 #LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
-LOCAL_PKG_LIST := qt5 openscenegraph freeglut
+LOCAL_PKG_LIST := qt5 openscenegraph freeglut protobuf
 .DEFAULT local-pkg-list:
 local-pkg-list: $(LOCAL_PKG_LIST)
 
