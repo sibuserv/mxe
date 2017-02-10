652402af23cbd7c187a75dbdea83510eb3e57338
Boris Pek <tehnick-8@yandex.ru>
2017-02-10 23:06:44 +0300

    Add settings.mk

---
+++ settings.mk
@@ -0,0 +1,40 @@
+# This is a template of configuration file for MXE. See
+# docs/index.html for more extensive documentations.
+
+# This variable controls the number of compilation processes
+# within one package ("intra-package parallelism").
+#JOBS := 
+
+# This variable controls where intermediate files are created
+# this is necessary when compiling inside a virtualbox shared
+# directory. Some commands like strip fail in there with Protocol error
+# default is the current directory
+#MXE_TMP := /tmp
+
+# This variable controls the targets that will build.
+#MXE_TARGETS := i686-w64-mingw32.static i686-w64-mingw32.shared x86_64-w64-mingw32.static x86_64-w64-mingw32.shared
+MXE_TARGETS := x86_64-w64-mingw32.static i686-w64-mingw32.static
+MXE_GCC_THREADS := posix
+
+# Special flags for compiler.
+CPPFLAGS := -D_FORTIFY_SOURCE=2
+CFLAGS   := -s -Os -fdata-sections -ffunction-sections -fPIC $(CPPFLAGS) -static-libgcc
+CXXFLAGS := -s -Os -fdata-sections -ffunction-sections -fPIC $(CPPFLAGS) -static-libgcc -static-libstdc++
+LDFLAGS  := -Wl,--gc-sections -Wl,--strip-all -Wl,--as-needed
+
+# This variable controls which plugins are in use.
+# See plugins/README.md for further information.
+#override MXE_PLUGIN_DIRS += plugins/apps plugins/native
+#MXE_PLUGIN_DIRS += plugins/gcc6
+
+# This variable controls the download mirror for SourceForge,
+# when it is used. Enabling the value below means auto.
+#SOURCEFORGE_MIRROR := downloads.sourceforge.net
+
+# The three lines below makes `make` build these "local
+# packages" instead of all packages.
+#LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
+LOCAL_PKG_LIST := aspell boost jsoncpp libidn lua miniupnpc nsis qtmultimedia qttools
+.DEFAULT local-pkg-list:
+local-pkg-list: $(LOCAL_PKG_LIST)
+
