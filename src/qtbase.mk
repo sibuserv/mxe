76b2d592827fafa4c3e2fb68de69ddde569934ca
Boris Pek <tehnick-8@yandex.ru>
2017-06-21 19:00:16 +0300

    qtbase: enable build of sqlite sql plugin

--- src/qtbase.mk
+++ src/qtbase.mk
@@ -9,7 +9,7 @@ $(PKG)_CHECKSUM := 6efa8a5c559e92b2e526d48034e858023d5fd3c39115ac1bfd3bb65834dbd
 $(PKG)_SUBDIR   := $(PKG)-opensource-src-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-opensource-src-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.qt.io/archive/qt/5.6/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc fontconfig freetype jpeg libpng openssl zlib
+$(PKG)_DEPS     := gcc fontconfig freetype jpeg libpng openssl sqlite zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- http://download.qt-project.org/official_releases/qt/5.6/ | \
@@ -47,14 +47,15 @@ define $(PKG)_BUILD
             -accessibility \
             -nomake examples \
             -nomake tests \
+            -plugin-sql-sqlite \
             -system-zlib \
             -system-libpng \
             -system-libjpeg \
+            -system-sqlite \
             -fontconfig \
             -system-freetype \
             -no-harfbuzz \
             -openssl-linked \
-            -no-sql-sqlite \
             -no-sql-sqlite2 \
             -no-sql-mysql \
             -no-sql-psql \
