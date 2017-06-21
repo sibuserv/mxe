74bf6a74ed3e878599808d781009432e7b2e8470
Boris Pek <tehnick-8@yandex.ru>
2017-06-21 19:19:53 +0300

    proj: update website URL

--- src/proj.mk
+++ src/proj.mk
@@ -1,7 +1,7 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := proj
-$(PKG)_WEBSITE  := https://trac.osgeo.org/proj/
+$(PKG)_WEBSITE  := http://proj4.org/
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 4.9.3
 $(PKG)_CHECKSUM := 6984542fea333488de5c82eea58d699e4aff4b359200a9971537cd7e047185f7
