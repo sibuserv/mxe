edd2232136631ebcd17f47d23a29ab65d7c711a3
Boris Pek <tehnick-8@yandex.ru>
2017-07-24 00:45:18 +0300

    Update settings.mk

--- settings.mk
+++ settings.mk
@@ -13,8 +13,7 @@
 
 # This variable controls the targets that will build.
 #MXE_TARGETS := i686-w64-mingw32.static i686-w64-mingw32.shared x86_64-w64-mingw32.static x86_64-w64-mingw32.shared
-#MXE_TARGETS := i686-w64-mingw32.shared x86_64-w64-mingw32.shared
-MXE_TARGETS := x86_64-w64-mingw32.shared
+MXE_TARGETS := i686-w64-mingw32.shared x86_64-w64-mingw32.shared
 MXE_GCC_THREADS := posix
 
 # Special flags for compiler.
@@ -35,7 +34,7 @@ MXE_PLUGIN_DIRS += plugins/gcc6
 # The three lines below makes `make` build these "local
 # packages" instead of all packages.
 #LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
-LOCAL_PKG_LIST := boost aspell hunspell jsoncpp libidn lua minizip miniupnpc nsis libotr tidy-html5 qtbase qttools qtmultimedia qtwebkit qca
+LOCAL_PKG_LIST := boost aspell hunspell jsoncpp libidn lua minizip miniupnpc nsis libotr tidy-html5 qtbase qttools qtmultimedia qca qtwebkit
 .DEFAULT_GOAL  := local-pkg-list
 local-pkg-list: $(LOCAL_PKG_LIST)
 
