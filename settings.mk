79858c515f7e2007d65f9283fdd225c6c011a31d
Boris Pek <tehnick-8@yandex.ru>
2017-11-27 00:43:43 +0300

    Update settings.mk

--- settings.mk
+++ settings.mk
@@ -34,7 +34,7 @@ MXE_PLUGIN_DIRS += plugins/gcc6
 # The three lines below makes `make` build these "local
 # packages" instead of all packages.
 #LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
-LOCAL_PKG_LIST := boost aspell hunspell jsoncpp libidn lua minizip miniupnpc libotr tidy-html5 gstreamer qtbase qttools qca qtmultimedia qtwebkit nsis libfcgi
+LOCAL_PKG_LIST := boost aspell hunspell jsoncpp libidn lua minizip miniupnpc libotr tidy-html5 gstreamer qtbase qttools qca qtmultimedia qtwebkit nsis
 .DEFAULT_GOAL  := local-pkg-list
 local-pkg-list: $(LOCAL_PKG_LIST)
 
