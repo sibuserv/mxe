05f3891a2806fcaec30cc088d8ade13b42eaeab7
Boris Pek <tehnick-8@yandex.ru>
2017-12-21 00:05:30 +0300

    osgearth: fix build

--- src/osgearth.mk
+++ src/osgearth.mk
@@ -6,9 +6,9 @@ $(PKG)_DESCR    := Geospatial SDK for OpenSceneGraph
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.8
 $(PKG)_CHECKSUM := 5570dc5b62f6f9e28954f5cbd7946a9b35767c06b375eff1c4cc40561e7f1655
-$(PKG)_GH_CONF  := gwaldron/osgearth,osgearth-
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+#$(PKG)_GH_CONF  := gwaldron/osgearth,osgearth-
+$(PKG)_SUBDIR   := osgearth-osgearth-$($(PKG)_VERSION)
+$(PKG)_FILE     := osgearth-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/gwaldron/osgearth/archive/$($(PKG)_FILE)
 $(PKG)_DEPS     := gcc gdal geos openscenegraph
 
