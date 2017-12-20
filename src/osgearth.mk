101bb8bf46cd111167d8a853255483692f17f484
Boris Pek <tehnick-8@yandex.ru>
2017-12-21 00:21:11 +0300

    osgearth: add build dependencies from curl, poco, sqlite, tinyxml, zlib

--- src/osgearth.mk
+++ src/osgearth.mk
@@ -10,14 +10,15 @@ $(PKG)_CHECKSUM := 5570dc5b62f6f9e28954f5cbd7946a9b35767c06b375eff1c4cc40561e7f1
 $(PKG)_SUBDIR   := osgearth-osgearth-$($(PKG)_VERSION)
 $(PKG)_FILE     := osgearth-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/gwaldron/osgearth/archive/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc gdal geos openscenegraph
+$(PKG)_DEPS     := curl gcc gdal geos openscenegraph poco sqlite tinyxml zlib
 
 define $(PKG)_UPDATE
     $(call MXE_GET_GITHUB_TAGS, gwaldron/osgearth, osgearth-)
 endef
 
 define $(PKG)_BUILD
-    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)'
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DWITH_EXTERNAL_TINYXML=ON
 
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
