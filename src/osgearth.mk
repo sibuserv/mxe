f6f1f552ee31e0cf75fa848695e1ce3a805c490b
Boris Pek <tehnick-8@yandex.ru>
2017-12-22 23:10:38 +0300

    osgearth: build without geos
    + set DYNAMIC_OSGEARTH configuration option
    + do not build examples

--- src/osgearth.mk
+++ src/osgearth.mk
@@ -10,7 +10,7 @@ $(PKG)_CHECKSUM := 5570dc5b62f6f9e28954f5cbd7946a9b35767c06b375eff1c4cc40561e7f1
 $(PKG)_SUBDIR   := osgearth-osgearth-$($(PKG)_VERSION)
 $(PKG)_FILE     := osgearth-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/gwaldron/osgearth/archive/$($(PKG)_FILE)
-$(PKG)_DEPS     := curl gcc gdal geos openscenegraph sqlite tinyxml zlib
+$(PKG)_DEPS     := curl gcc gdal openscenegraph sqlite tinyxml zlib
 
 define $(PKG)_UPDATE
     $(call MXE_GET_GITHUB_TAGS, gwaldron/osgearth, osgearth-)
@@ -18,7 +18,9 @@ endef
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(TARGET)-cmake '$(SOURCE_DIR)' \
-        -DWITH_EXTERNAL_TINYXML=ON
+        -DWITH_EXTERNAL_TINYXML=ON \
+        -DDYNAMIC_OSGEARTH=$(CMAKE_SHARED_BOOL) \
+        -DBUILD_OSGEARTH_EXAMPLES=OFF
 
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
