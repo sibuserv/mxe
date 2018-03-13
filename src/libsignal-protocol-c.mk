ff06d28fe5aeb8074e4e7a6d7ea179a70302c634
Boris Pek <tehnick-8@yandex.ru>
2018-03-13 21:23:46 +0300

    libsignal-protocol-c: use git snapshots
    until new stable version is released

--- src/libsignal-protocol-c.mk
+++ src/libsignal-protocol-c.mk
@@ -4,10 +4,15 @@ PKG             := libsignal-protocol-c
 $(PKG)_WEBSITE  := https://github.com/signalapp/libsignal-protocol-c
 $(PKG)_DESCR    := libsignal-protocol-c
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.3.1
-$(PKG)_CHECKSUM := 1afc195cc87153ea5178b485a2bf9f4791c03fd70c9b2e3c1ecc55bbb64c9fce
-$(PKG)_GH_CONF  := signalapp/libsignal-protocol-c/tags, v
+$(PKG)_VERSION  := 9e10362fce9072b104e6d5a51d6f56d939d1f36e
+$(PKG)_CHECKSUM := e97e9e707a60e21ef85634e2de9b1360c9830793cc3e8ae5fc58008fd3565f75
+$(PKG)_SUBDIR   := libsignal-protocol-c-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://github.com/signalapp/libsignal-protocol-c/archive/$($(PKG)_VERSION).tar.gz
+#$(PKG)_GH_CONF  := signalapp/libsignal-protocol-c/tags, v
 $(PKG)_DEPS     := cc pthreads
+
+$(PKG)_UPDATE    = $(call MXE_GET_GITHUB_SHA, signalapp/libsignal-protocol-c, master)
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
