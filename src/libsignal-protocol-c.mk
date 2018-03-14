e6a6da9f35bb1863635d313abba340bf5977ce93
Boris Pek <tehnick-8@yandex.ru>
2018-03-15 00:43:03 +0300

    libsignal-protocol-c: use new GH_CONF format

--- src/libsignal-protocol-c.mk
+++ src/libsignal-protocol-c.mk
@@ -4,12 +4,9 @@ PKG             := libsignal-protocol-c
 $(PKG)_WEBSITE  := https://github.com/signalapp/libsignal-protocol-c
 $(PKG)_DESCR    := libsignal-protocol-c
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 9e10362fce9072b104e6d5a51d6f56d939d1f36e
-$(PKG)_CHECKSUM := e97e9e707a60e21ef85634e2de9b1360c9830793cc3e8ae5fc58008fd3565f75
-$(PKG)_SUBDIR   := libsignal-protocol-c-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/signalapp/libsignal-protocol-c/archive/$($(PKG)_VERSION).tar.gz
-#$(PKG)_GH_CONF  := signalapp/libsignal-protocol-c/tags, v
+$(PKG)_VERSION  := 9e10362
+$(PKG)_CHECKSUM := 05a6161cca61e16679fe189039b3cdebe26ed7ee8414d59afdc60363f46fb8fe
+$(PKG)_GH_CONF  := signalapp/libsignal-protocol-c/branches/master
 $(PKG)_DEPS     := cc pthreads
 
 $(PKG)_UPDATE    = $(call MXE_GET_GITHUB_SHA, signalapp/libsignal-protocol-c, master)
