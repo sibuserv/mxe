6b9d04ba3cdc01035586113e0b07674c3043ff0f
Boris Pek <tehnick-8@yandex.ru>
2018-10-09 01:14:17 +0300

    update openscenegraph

--- src/openscenegraph.mk
+++ src/openscenegraph.mk
@@ -4,8 +4,8 @@ PKG             := openscenegraph
 $(PKG)_WEBSITE  := http://www.openscenegraph.org/
 $(PKG)_DESCR    := OpenSceneGraph
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.4.1
-$(PKG)_CHECKSUM := 930eb46f05781a76883ec16c5f49cfb29a059421db131005d75bec4d78401fd5
+$(PKG)_VERSION  := 3.6.3
+$(PKG)_CHECKSUM := 51bbc79aa73ca602cd1518e4e25bd71d41a10abd296e18093a8acfebd3c62696
 $(PKG)_GH_CONF  := openscenegraph/OpenSceneGraph/tags, OpenSceneGraph-
 $(PKG)_DEPS     := cc freetype curl gdal giflib jpeg libpng openthreads tiff zlib
 
@@ -18,14 +18,16 @@ endef
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
-        -DCMAKE_CXX_FLAGS="$(CXXFLAGS) -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS" \
+        -DCMAKE_CXX_FLAGS='$(CXXFLAGS) -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS' \
         -DCMAKE_HAVE_PTHREAD_H=OFF \
+        -DOSG_DETERMINE_WIN_VERSION=OFF \
         -DPKG_CONFIG_EXECUTABLE='$(PREFIX)/bin/$(TARGET)-pkg-config' \
         -DDYNAMIC_OPENTHREADS=$(CMAKE_SHARED_BOOL) \
         -DDYNAMIC_OPENSCENEGRAPH=$(CMAKE_SHARED_BOOL) \
         -DBUILD_OSG_APPLICATIONS=OFF \
         -D_OPENTHREADS_ATOMIC_USE_GCC_BUILTINS_EXITCODE=1 \
         -D_OPENTHREADS_ATOMIC_USE_WIN32_INTERLOCKED=1
+
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
 endef
