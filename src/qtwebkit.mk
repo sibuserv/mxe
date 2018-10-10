9a40aeb496cd952ebfaf42995edb989ff84d28f9
Boris Pek <tehnick-8@yandex.ru>
2018-10-11 01:13:47 +0300

    update qtwebkit

--- src/qtwebkit.mk
+++ src/qtwebkit.mk
@@ -1,19 +1,13 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := qtwebkit
-$(PKG)_WEBSITE  := https://www.qt.io/
-$(PKG)_DESCR    := Qt
+$(PKG)_WEBSITE  := https://github.com/annulen/webkit
+$(PKG)_DESCR    := Qt WebKit
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.9.1
-$(PKG)_CHECKSUM := 28a560becd800a4229bfac317c2e5407cd3cc95308bc4c3ca90dba2577b052cf
-$(PKG)_SUBDIR   := $(PKG)-opensource-src-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-opensource-src-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := https://download.qt.io/official_releases/qt/5.9/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
+$(PKG)_VERSION  := bd0657f
+$(PKG)_CHECKSUM := fdf81621440999f95fe1cad96525446d1e961665ef7cf319259c6c826a495f65
+$(PKG)_GH_CONF  := qt/qtwebkit/branches/5.9
 $(PKG)_DEPS     := cc qtbase qtmultimedia qtquickcontrols sqlite
-
-define $(PKG)_UPDATE
-    echo $(qtbase_VERSION)
-endef
 
 define $(PKG)_BUILD_SHARED
     cd '$(BUILD_DIR)' && '$(PREFIX)/$(TARGET)/qt5/bin/qmake' '$(SOURCE_DIR)'
