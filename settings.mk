1daf7959f3e154ae1c081b48e67abfd84898aa29
Boris Pek <tehnick-8@yandex.ru>
2019-01-09 01:50:10 +0300

    Update settings.

--- settings.mk
+++ settings.mk
@@ -15,19 +15,11 @@
 #MXE_TARGETS := i686-w64-mingw32.static i686-w64-mingw32.shared \
 #               x86_64-w64-mingw32.static x86_64-w64-mingw32.shared
 MXE_TARGETS := x86_64-w64-mingw32.shared i686-w64-mingw32.shared
-MXE_GCC_THREADS := posix
-
-# Special flags for compiler.
-#CPPFLAGS := -D_FORTIFY_SOURCE=2
-#CFLAGS   := -s -Os -fdata-sections -ffunction-sections -fPIC $(CPPFLAGS) -static-libgcc
-#CXXFLAGS := -s -Os -fdata-sections -ffunction-sections -fPIC $(CPPFLAGS) -static-libgcc -static-libstdc++
-#LDFLAGS  := -Wl,--gc-sections -Wl,--strip-all -Wl,--as-needed
 
 # This variable controls which plugins are in use.
 # See plugins/README.md for further information.
 #override MXE_PLUGIN_DIRS += plugins/apps plugins/native
-MXE_PLUGIN_DIRS += plugins/gcc8
-#MXE_PLUGIN_DIRS += plugins/examples/qt5-freeze
+MXE_PLUGIN_DIRS += plugins/gcc6
 
 # This variable controls the download mirror for SourceForge,
 # when it is used. Enabling the value below means auto.
@@ -38,7 +30,7 @@ MXE_PLUGIN_DIRS += plugins/gcc8
 #LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
 LOCAL_PKG_LIST := boost aspell hunspell jsoncpp libidn lua minizip miniupnpc \
                   libfcgi libotr libsignal-protocol-c tidy-html5 qtbase \
-                  qttools qtkeychain qca qtmultimedia gstreamer \
+                  qttools qtkeychain qca qtmultimedia qtwebkit gstreamer \
                   gst-plugins-bad gst-plugins-good
 
 .DEFAULT_GOAL  := local-pkg-list
