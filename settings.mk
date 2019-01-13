3b1c63b487188de4c5e1b267149606b3ba41b99d
Boris Pek <tehnick-8@yandex.ru>
2019-01-14 01:04:20 +0300

    Revert dfd9ba55f4c8612f5ec999307c6742b1336e510e.

--- settings.mk
+++ settings.mk
@@ -30,9 +30,8 @@ MXE_PLUGIN_DIRS += plugins/gcc6
 #LOCAL_PKG_LIST := boost curl file flac lzo pthreads vorbis wxwidgets
 LOCAL_PKG_LIST := boost aspell hunspell jsoncpp libidn lua minizip miniupnpc \
                   libfcgi libotr libsignal-protocol-c tidy-html5 qtbase \
-                  qtwinextras qtimageformats qttools qtkeychain qca \
-                  qtmultimedia qtwebkit gstreamer gst-plugins-bad \
-                  gst-plugins-good
+                  qttools qtkeychain qca qtmultimedia qtwebkit gstreamer \
+                  gst-plugins-bad gst-plugins-good
 
 .DEFAULT_GOAL  := local-pkg-list
 local-pkg-list: $(LOCAL_PKG_LIST)
